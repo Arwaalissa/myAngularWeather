e33a808bcc8daf6fdea5a8a7fd53619e3707ca1d
Arwaalissa <43367580+Arwaalissa@users.noreply.github.com>
2019-10-27 18:20:22 +0300

    adding key and map

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import {AgmCoreModule} from '@agm/core';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    FormsModule, ] })
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDH_-oWc4qBNezLOGx7o6-rjba9lZX2sik'
+    }),
+   ],
+  exports: [
+    FormsModule,
+    AgmCoreModule,
+  ] })
 export class SharedModule { }
